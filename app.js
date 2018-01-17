8fabd01049826722e7db1bc91cfb9ce128b6e3ec
jwpsr <jwparkersr@gmail.com>
2018-01-16 22:24:12 -0600

    added heroku port into

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
+const PORT = process.env.PORT || 8080
 
 express ()
     .get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')))
-    .listen(8080, () => console.log("Server running."));
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
